d1f2f982b9abba8923c725f21b52a8fb4d1dceba
Relyson Ramos <relyson.l.m.ramos@outlook.com>
2022-07-03 18:58:08 -0300

    Script scroll animado footer

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function footerAnimate() {
+    const rodapeH = document.querySelector('footer').getBoundingClientRect().top;
+    const elementoRodape = document.getElementById("footerAnime");
+
+    rodapeH < window.innerHeight ? elementoRodape.className = 'autor animaRodape' : elementoRodape.className = 'autor';
+}
+
+
